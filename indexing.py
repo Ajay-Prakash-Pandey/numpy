d2ad3458ef1f7bf0834d042149d4ed6d80da8f0d
Ajay-Prakash-Pandey <ajayprakashp59@gmail.com>
2025-08-05 15:05:12 +0530

    i complete indexing in numpy

--- indexing.py
+++ indexing.py
@@ -1,10 +1,27 @@
-# import numpy as np
+#Array indexing is same as accessing array element
 
-# s=np.array([1,2,3,4,5])
-# # print(s[0])
-# print(list(s))
-# for i in s:
-#     print(i)
+# 1 d array 
+import numpy as np
+array1=np.array([1,2,3,4])
 
-# 
-int("abc")
+print(array1[0])
+
+# we can get two element for adding them
+print(array1[2]+array1[3])
+
+for ele in array1:
+    print(ele)
+
+# 2 d array 
+
+array2d=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print("Second element in the 1st row",array2d[0][1])
+
+# for row in array2d:
+#     for col in array2d:
+#         print(array2d[row][col])
+
+
+array3d=np.array([[[1,2,3],[4,5,6],[7,8,9]],[[9,8,7],[6,5,4],[3,2,1]],[[1,2,3],[4,5,6],[7,8,9]],[[9,8,7],[6,5,4],[3,2,1]]])
+print(array3d[1][0][0])
+
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 <!-- here i am learning numpy  -->
 i acn learn some topics in numpy such are
-1-> introduction to numpy
-2->indixing in numpy
-3> creating array 
+1->introduction to numpy
+2->creating array 
+3>indixing in numpy 
