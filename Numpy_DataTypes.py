842d1c6062c313c261061902171aa54981ae25c7
Ajay-Prakash-Pandey <ajayprakashp59@gmail.com>
2025-08-05 16:47:35 +0530

    adding without conpleting numpyDataTypes

---
+++ Numpy_DataTypes.py
@@ -0,0 +1,20 @@
+# datatypes in numpy
+'''
+datatypes are :
+i - integer
+b - boolean
+u - unsigned integer
+f - float
+c - complex float
+m - timedelta
+M - datetime
+O - object
+S - string
+U - unicode string
+V - fixed chunk of memory for other type ( void )
+
+'''
+
+import numpy as np
+data1=np.array([1,2,3,4,5,6,7,8,9])
+print(type(data1))
